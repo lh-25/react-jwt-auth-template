0295e4fe672a97a449c382995868f45e1584c9ac
lh-25 <sprinklecake25@gmail.com>
2024-11-13 13:29:49 -0600

    user signin functionality coomplete

--- src/services/authService.js
+++ src/services/authService.js
@@ -5,9 +5,28 @@ const BACKEND_URL = import.meta.env.VITE_EXPRESS_BACKEND_URL;
 const signup = async (formData) => {
   try {
     const res = await axios.post(`${BACKEND_URL}/users/signup`, formData)
-    if (res.error) {
-      throw new Error(res.error);
+    if (res.data.error) {
+      throw new Error(res.data.error);
 
+    }
+    return res.data
+
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+const signin = async (user) => {
+  try {
+    const res = await axios.post(`${BACKEND_URL}/users/signin`, user)
+    if (res.data.error) {
+      throw new Error(res.data.error);
+    }
+
+    if (res.data.token) {
+      const user = JSON.parse(atob(res.data.token.split('.')[1]))
+      return user
     }
     return res.data
 
@@ -20,5 +39,5 @@ const signup = async (formData) => {
 
 export {
   signup,
-
+  signin
 }
