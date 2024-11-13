4ad22c3c6b4049bf59801382d1a0bc9e274a990b
lh-25 <sprinklecake25@gmail.com>
2024-11-13 11:51:45 -0600

    finished NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,24 @@
-const NavBar = ({ user }) => {
+import { Link } from "react-router-dom"
+
+export default function NavBar({ user }) {
   return (
     <>
-      <p>Hello Navbar</p>
+      {user ? (
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="" >Sign Out</Link></li>
+          </ul>
+        </nav>
+      ) : (
+        <nav>
+          <ul>
+            <li><Link to="/signin">Sign In</Link></li>
+            <li><Link to="/signup" >Sign Up</Link></li>
+          </ul>
+        </nav>
+      )}
     </>
   )
 }
-export default NavBar
+
